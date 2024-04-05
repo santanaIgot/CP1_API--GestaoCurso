4e3ea4c6299d123d20474a8321a6a958a6ed6165
LucasSpinelli003 <rm97757@fiap.com.br>
2024-04-04 23:43:33 -0300

    [Fix] - Matricula modal - fixed createdDate column

--- GestaoCurso/src/main/java/br/com/fiap/GestaoCurso/model/Matricula.java
+++ GestaoCurso/src/main/java/br/com/fiap/GestaoCurso/model/Matricula.java
@@ -5,14 +5,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @NoArgsConstructor
+
 @Entity
 @Table(name = "Matricula_cp1_java")
+@EntityListeners(AuditingEntityListener.class)
 public class Matricula {
+
     @Id
     @GeneratedValue
     @Column(name = "cd_matricula")
@@ -25,6 +30,7 @@ public class Matricula {
     private Long codigoCurso;
 
     @Column(name = "dt_matricula")
+    @CreatedDate
     private LocalDateTime dataMatricula;
 
     @Column(name = "nr_rm")
